320f4295912190a10b4c4c0b4661b627015547dc
lagodukn <thiagolagaresgon@gmail.com>
2023-09-13 21:28:27 -0300

    some changes

--- pages/index.js
+++ pages/index.js
@@ -14,14 +14,35 @@ const Home = () => {
     <div className='w-full h-full bg-gradient-t-r from-primary/10 via/black/30 to-black/10'>
       <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
         {/* {title} */}
-        <h1 className='h1'>
-          Titulo de impacto <br /> Com{' '}
-          <span className='text-yellow-200'>Destaque Colorido</span>
-        </h1>
+        <motion.h1 variants={fadeIn('down', 0.4)}
+          initial='hidden'
+          animate='show'
+          exit='hidden'
+          className='h1 uppercase text-slate-50	'>
+          Titulo de impacto Com <br />{' '}
+          <span className='uppercase font-bold text-black'>Destaque </span><span className='uppercase font-bold text-yellow-200'>Colorido</span> <br />
+        </motion.h1>
         {/* {subtitulo} */}
-        <p className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>
+        <motion.p variants={fadeIn('down', 0.4)}
+          initial='hidden'
+          animate='show'
+          exit='hidden'
+          className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>
           Aqui pode ter uma descrição simples com muitos lorems imsado asda dasm  maomdwaida mdaowdmawd hu hdhua dhua ha huiwd a nadun au ndau au uaua  ndaiuwduyadm md aoimw
-        </p>
+        </motion.p>
+        {/* {btn} */}
+        <div className='flex justify-center xl:hidden'>
+          <ProjectsBtn />
+        </div>
+        <motion.div 
+          variants={fadeIn('down', 0.4)}
+          initial='hidden'
+          animate='show'
+          exit='hidden'
+          className='hidden xl:flex'
+        >
+          <ProjectsBtn />
+        </motion.div>
       </div>
     </div>
 
