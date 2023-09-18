e88ff92e9af6f418c47d1e8ba10bd7062c121794
lagodukn <thiagolagaresgon@gmail.com>
2023-09-18 13:06:48 -0300

    services and about

--- pages/services/index.js
+++ pages/services/index.js
@@ -3,8 +3,8 @@ import ServiceSlider from '../../components/ServiceSlider'
 
 const Services = () => {
   return <div className='bg-primary/60 h-[85vh] xl:h-[90vh] m-4 rounded-3xl'>
-    <div className='container mx-auto'>
-      <div className='flex flex-col items-center justify-center xl:flex-row gap-x-8'>
+    <div className='container mx-auto h-full flex flex-col justify-center'>
+      <div className='flex flex-col xl:flex-row gap-x-8'>
         {/* {text} */}
         <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
           <h2 className='h2 xl:mt-8'>
@@ -18,8 +18,10 @@ const Services = () => {
             aiksmdaosdasmd
           </p>
         </div>
-        {/* {slide} */}
-        <ServiceSlider />
+        <div className='h-full xl:max-w-[65%]'>
+          {/* {slide} */}
+          <ServiceSlider />
+        </div>
       </div>
     </div>
   </div>
